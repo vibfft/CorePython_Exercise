7ef782aa8abd618d8c95f3016216f1891499ae56
Stephen Moon <stephen.moon@gmail.com>
2023-03-24 16:15:20 -0700

    add authenticate function

---
+++ authenticate.py
@@ -0,0 +1,18 @@
+# Create an @authenticated decorator that only allows the function to run is user1 has 'valid' set to True:
+user1 = {
+    'name': 'Sorna',
+    'valid': True 
+}
+
+def authenticated(fn):
+  def wrapper_func(user):
+      if user['valid']:
+        fn(user)
+  return wrapper_func
+      
+
+@authenticated
+def message_friends(user):
+    print(f"The message has been sent from {user['name']}")
+
+message_friends(user1)
