cecabfc3900ddf606d8da1db92e1ed3bbe6568cc
vibfft <vibfft@gmail.com>
2023-06-01 17:35:42 -0700

    add coin change problem

---
+++ leet_code/coin_change/can_get_exact_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+def canGetExactChange(targetMoney, denominations):
+
+    if targetMoney in denominations:
+        return True
+    else:
+        coin_list = sorted(denominations, reverse=True)
+        for c in coin_list:
+            if targetMoney == c:
+                return True
+            else:
+                if c < targetMoney:
+                    coin_list.remove(c)
+                    return canGetExactChange(targetMoney - c, denominations)
+        else:
+            return False
+
+
+print(canGetExactChange(94, [5, 10, 25, 100, 200]))
+print(canGetExactChange(75, [4, 17, 29]))
+print(canGetExactChange(25, [5, 10, 25]))
